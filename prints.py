e0daab7382468b1fc241a23c89e072fb6f1ac301
Wainainajoyce <joycewainaina19@gmail.com>
2023-08-03 21:36:17 +0300

    First commit

---
+++ prints.py
@@ -0,0 +1,17 @@
+#Hello World
+print("Hello World!!")
+#personal data
+print("Joyce Wainaina")
+print("30 years old haha")
+print("I expect to be able to comfortably build a python app from scratch")
+
+# A function is a template to create objects.
+def add_numbers(a , b):
+    result = a + b 
+    return result
+age1 = 45
+age2 = 50
+ 
+ages = add_numbers(age1 ,age2)
+print(ages)
+
---
+++ variables.py
@@ -0,0 +1,11 @@
+#Understanding variables in python
+name = "Joyce"
+age = 40
+is_tall = False
+PI = 3.142
+#can assign multiple variables on a single line
+pet,cartype,hobby ="dog","lexus","swimming"
+print(cartype)
+#why learn coding?
+reason = "Improve coding skills"
+print(reason)
