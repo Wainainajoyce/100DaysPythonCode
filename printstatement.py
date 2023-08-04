068cd2ac28a717e1759728af13916a75249d80e6
Wainainajoyce <joycewainaina19@gmail.com>
2023-08-04 21:42:06 +0300

    Day 2 assignments

---
+++ printstatement.py
@@ -0,0 +1,13 @@
+#These are print statements
+print("My name is Candy")
+print("""
+I am a very smart lady
+I speak English and French
+I am from Kenya.
+""")
+course = "Software Development"
+print(course)
+name = input("Enter your name: ")
+print(name)
+grade = input("Enter the grade you are in: ")
+
---
+++ variables1.py
@@ -0,0 +1,6 @@
+name = "randy"
+age = 5
+country = "Uganda"
+print(name)
+print(age)
+print(country)
