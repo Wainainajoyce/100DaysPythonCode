8a86d1d3442277f3682ec6d5cff88a8503b41510
Wainainajoyce <joycewainaina19@gmail.com>
2023-08-04 21:41:18 +0300

    2 commit for day 1 assignment

---
+++ arithmetic.py
@@ -0,0 +1,40 @@
+#Performing addition
+age1 = 50
+age2 = 70
+
+total_age = age1 + age2
+print(total_age)
+
+#Performing substraction
+diff_age = age2 - age1
+print(diff_age)
+
+#Performing division
+a = 90
+b = 5
+division = a // b
+print(division)
+
+#Performing multiplication
+multiple = a*b
+print(multiple)
+
+#Performing modulus
+c = 99
+d = 5
+mod = c%d
+print(mod)
+
+#power form
+power = d ** 2
+print(power)
+
+#squareroot
+import math
+sroot = math.sqrt(c)
+
+
+
+
+
+
---
+++ variables2.py
@@ -0,0 +1,6 @@
+is_female = True
+k , l , m = "Hello" , "Goodmorning" , "Goodnight"
+print(k)
+print(l)
+print(m)
+print(is_female)
