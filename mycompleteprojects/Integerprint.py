677101b49e6d0a9232bc9cda2cf53743987d1588
Wainainajoyce <joycewainaina19@gmail.com>
2023-08-23 15:01:50 +0300

    Many commits

---
+++ mycompleteprojects/Integerprint.py
@@ -0,0 +1,8 @@
+#The following program receives a number n and outputs all the preceding numbers
+if __name__ == '__main__':
+    n = input("Enter number n: ")
+# def number(n):
+output = range(1,n+1)
+print(list(output))
+
+
